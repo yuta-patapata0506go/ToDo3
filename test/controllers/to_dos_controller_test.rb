5f770a95b9bfabcea17d1a6d12e5a28a433c2d3c
奥畑雄太 <oku.cheko@gmail.com>
2025-09-12 21:20:09 +0900

    Fix to_dos.yml

--- test/controllers/to_dos_controller_test.rb
+++ test/controllers/to_dos_controller_test.rb
@@ -1,25 +1,3 @@
 require "test_helper"
 
 class ToDosControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get to_dos_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_to_do_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    to_do = to_dos(:one) # fixtureから取得（適宜修正）
-    get to_do_url(to_do)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    to_do = to_dos(:one)
-    get edit_to_do_url(to_do)
-    assert_response :success
-  end
-end
