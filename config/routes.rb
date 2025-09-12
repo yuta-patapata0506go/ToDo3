36acfb60fbe17d71d9db6ca855481b1b1e5be574
奥畑雄太 <oku.cheko@gmail.com>
2025-09-12 20:52:19 +0900

    Fix little

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "users/show"
   get "users/new"
   get "users/edit"
-  root to:'to_dos#index'
+  root to:"to_dos#index"
   resources :to_dos
 
   get "up" => "rails/health#show", as: :rails_health_check
